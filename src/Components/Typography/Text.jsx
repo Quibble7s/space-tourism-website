878e36a88405f26a54e45e859553223a2788e942
Cesar Gallardo <wuelo789@gmail.com>
2022-03-02 18:26:05 -0500

    Changed the Text component so it uses the correct line height in small screen sizes

--- src/Components/Typography/Text.jsx
+++ src/Components/Typography/Text.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Text = ({ children, className = '' }) => {
   return (
     <p
-      className={`text-white font-barlow text-[15px] md:text-[18px] md:leading-[132%] ${className}`}>
+      className={`text-white font-barlow text-[15px] leading-[25px] md:text-[18px] md:leading-[32px] ${className}`}>
       {children}
     </p>
   );
