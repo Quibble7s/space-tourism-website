4aa5c45d11639969e3efd43fbffb2f417e044fb6
Cesar Gallardo <wuelo789@gmail.com>
2022-03-02 15:47:41 -0500

    Added initial config for screen sizes and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1440px',
+    },
+    colors: { darkBlue: '#0B0D17', lightPurple: '#D0D6F9' },
+    fontFamily: {
+      barlow: ['Barlow Condensed', 'sans-serif'],
+      bellefair: ['Bellefair', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
