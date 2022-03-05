c15f0968210b4cf6009cb16ac8fbf0557cf272e9
Cesar Gallardo <wuelo789@gmail.com>
2022-03-04 20:44:42 -0500

    Fixed a problem where the text was not displaying the correct size

--- src/Components/Typography/NavText.jsx
+++ src/Components/Typography/NavText.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const NavText = ({ children, className = '' }) => {
   return (
     <p
-      className={`text-white font-barlow text-[16px]tracking-[2.7px] ${className}`}>
+      className={`text-white font-barlow text-[16px] tracking-[2.7px] ${className}`}>
       {children}
     </p>
   );
