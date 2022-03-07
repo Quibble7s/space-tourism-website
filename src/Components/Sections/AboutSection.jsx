c1bc3ca2f1a45aa59165ed541afaa274a1e28d08
Cesar Gallardo <wuelo789@gmail.com>
2022-03-06 19:32:40 -0500

    Fixed an issue where the text was not displaying the correct line height in mobile view

--- src/Components/Sections/AboutSection.jsx
+++ src/Components/Sections/AboutSection.jsx
@@ -5,11 +5,11 @@ import Text from '../Typography/Text';
 const AboutSection = () => {
   return (
     <section className='mx-[24px] mt-[48px]'>
-      <Heading type={5} className='uppercase text-center'>
+      <Heading type={5} className='uppercase text-center leading-[19.2px]'>
         So, you want to travel to
       </Heading>
-      <Heading className='mt-4 text-center'>SPACE</Heading>
-      <Text className='mt-4 text-center text-light-purple'>
+      <Heading className='mt-[16px] text-center leading-[100px]'>SPACE</Heading>
+      <Text className='mt-[16px] min-h-[125px] text-center text-light-purple leading-[25px]'>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we’ll give you a truly out of this world
