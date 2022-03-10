048d86963e06f03f0491313aa10a1a2934a40a3b
Cesar Gallardo <wuelo789@gmail.com>
2022-03-09 23:57:44 -0500

    Added logo component

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+
+const Logo = ({ width = 0, height = 0, className = '' }) => {
+  return (
+    <div className={className}>
+      <Image
+        width={width}
+        height={height}
+        objectFit='contain'
+        src='/assets/shared/logo.svg'
+      />
+    </div>
+  );
+};
+
+Logo.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  className: PropTypes.string,
+};
+
+export default Logo;
