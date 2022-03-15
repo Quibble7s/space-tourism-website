effe49b8b667b73c2f3da6e0e67fdd0ff3d5488b
Cesar Gallardo <wuelo789@gmail.com>
2022-03-14 19:32:10 -0500

    Added destination button

---
+++ src/Components/Buttons/DestinationButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//Components
+import NavText from '../Typography/NavText';
+
+//Styles
+import Styles from './Styles/destination-button.module.css';
+
+const DestinationButton = ({
+  onClick = () => null,
+  selectedDestination = '',
+  children,
+  ...rest
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`cursor-pointer ${Styles.button}`}
+      {...rest}>
+      <NavText className='tracking-[2.36px] leading-[16.8px] pointer-events-none'>
+        {children}
+      </NavText>
+      <div
+        className={`w-full min-h-[3px] mt-[8px] transition-colors pointer-events-none ${
+          selectedDestination === rest.name ? Styles.active : ''
+        } ${Styles.indicator}`}
+      />
+    </button>
+  );
+};
+
+DestinationButton.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.string,
+  selectedDestination: PropTypes.string,
+};
+
+export default DestinationButton;
