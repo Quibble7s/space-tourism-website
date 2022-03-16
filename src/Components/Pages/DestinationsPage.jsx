8b9f22b6557c31a47e409cf6d03d46831ae3b27e
Cesar Gallardo <wuelo789@gmail.com>
2022-03-15 19:40:43 -0500

    Added the necesary classes to match the tablet desing

--- src/Components/Pages/DestinationsPage.jsx
+++ src/Components/Pages/DestinationsPage.jsx
@@ -12,6 +12,9 @@ import destinationData from '../../Data/data.json';
 import DestinationButton from '../Buttons/DestinationButton';
 
 const DestinationsPage = () => {
+  //Destinations
+  const destinations = ['moon', 'mars', 'europa', 'titan'];
+
   //Hooks
   const [selectedBody, setSelectedBody] = useState('moon');
   const data = destinationData.destinations.filter(
@@ -31,18 +34,18 @@ const DestinationsPage = () => {
   };
 
   return (
-    <div
+    <section
       className={`w-full min-h-screen bg-cover overflow-x-hidden ${mobileContainer} ${tabletContainer} ${desktopContainer}`}>
       <Nav />
-      <div className='mx-[24px]'>
+      <div className='mx-[24px] md:ml-[39px]'>
         <div>
           <Heading
             type={5}
-            className='text-center text-[#FFF] leading-[19.2px] mt-[24px]'>
+            className='text-center text-[#FFF] leading-[19.2px] mt-[24px] md:text-left md:mt-[40px] md:tracking-[3.38px] md:leading-[24px]'>
             <span className='mr-[18px] text-white/25'>01</span> PICK YOUR
             DESTINATION
           </Heading>
-          <div className='max-w-[170px] max-h-[170px] mx-auto mt-[32px]'>
+          <div className='max-w-[170px] max-h-[170px] mx-auto mt-[32px] md:max-w-[300px] md:max-h-[300px] md:mt-[60px]'>
             <Image
               width={445}
               height={445}
@@ -51,66 +54,54 @@ const DestinationsPage = () => {
           </div>
         </div>
         <div className='mb-[58px]'>
-          <div className='mt-[26px] flex flex-row justify-center items-center gap-[26.5px]'>
-            <DestinationButton
-              onClick={onDestinationClickHandler}
-              selectedDestination={selectedBody}
-              name='moon'>
-              MOON
-            </DestinationButton>
-            <DestinationButton
-              onClick={onDestinationClickHandler}
-              selectedDestination={selectedBody}
-              name='mars'>
-              MARS
-            </DestinationButton>
-            <DestinationButton
-              onClick={onDestinationClickHandler}
-              selectedDestination={selectedBody}
-              name='europa'>
-              EUROPA
-            </DestinationButton>
-            <DestinationButton
-              onClick={onDestinationClickHandler}
-              selectedDestination={selectedBody}
-              name='titan'>
-              TITAN
-            </DestinationButton>
+          <div className='mt-[26px] flex flex-row justify-center items-center gap-[26.5px] md:mt-[53px]'>
+            {destinations.map((destination) => (
+              <DestinationButton
+                onClick={onDestinationClickHandler}
+                selectedDestination={selectedBody}
+                name={destination}>
+                {destination.toUpperCase()}
+              </DestinationButton>
+            ))}
           </div>
-          <Heading type={2} className='text-center leading-[64.18px] mt-[20px]'>
+          <Heading
+            type={2}
+            className='text-center leading-[64.18px] mt-[20px] md:mt-[32px] md:leading-[92px]'>
             {selectedBody?.toUpperCase()}
           </Heading>
-          <Text className='text-center text-light-purple'>
+          <Text className='text-center text-light-purple md:max-w-[573px] md:min-h-[84px] md:mx-auto md:mt-8px'>
             {data[0]?.description}
           </Text>
-          <span className='block w-full min-h-[1px] bg-[#383B4B] mt-[32px]' />
-          <div>
-            <SubHeading
-              type={2}
-              className='text-light-purple leading-[16.8px] text-center mt-[32px] tracking-[2.36px]'>
-              AVG. DISTANCE
-            </SubHeading>
-            <SubHeading
-              type={1}
-              className='text-center leading-[32.09px] mt-[12px]'>
-              {data[0]?.distance.toUpperCase()}
-            </SubHeading>
-          </div>
-          <div>
-            <SubHeading
-              type={2}
-              className='text-light-purple leading-[16.8px] text-center mt-[32px] tracking-[2.36px]'>
-              EST. TRAVEL TIME
-            </SubHeading>
-            <SubHeading
-              type={1}
-              className='text-center leading-[32.09px] mt-[12px]'>
-              {data[0]?.travel.toUpperCase()}
-            </SubHeading>
+          <span className='block w-full min-h-[1px] bg-[#383B4B] mt-[32px] md:mt-[49px]' />
+          <div className='flex flex-col mt-[32px] md:flex-row md:justify-center md:items-center md:gap-[11px] md:mt-[28px]'>
+            <div className='md:min-w-[216px]'>
+              <SubHeading
+                type={2}
+                className='text-light-purple leading-[16.8px] text-center tracking-[2.36px]'>
+                AVG. DISTANCE
+              </SubHeading>
+              <SubHeading
+                type={1}
+                className='text-center leading-[32.09px] mt-[12px]'>
+                {data[0]?.distance.toUpperCase()}
+              </SubHeading>
+            </div>
+            <div className='md:min-w-[223px]'>
+              <SubHeading
+                type={2}
+                className='text-light-purple leading-[16.8px] text-center tracking-[2.36px] mt-[32px] md:mt-0'>
+                EST. TRAVEL TIME
+              </SubHeading>
+              <SubHeading
+                type={1}
+                className='text-center leading-[32.09px] mt-[12px]'>
+                {data[0]?.travel.toUpperCase()}
+              </SubHeading>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
